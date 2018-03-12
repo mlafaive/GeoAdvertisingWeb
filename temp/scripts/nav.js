4a0951ac18a64a7dfea0c7fbf99404f2a5cccaba
kwokh <kwokh@umich.edu>
2018-03-12 18:25:09 -0400

    add contact page

---
+++ temp/scripts/nav.js
@@ -0,0 +1,35 @@
+var sections = [$("#landing"), $("#about"), $("#contact")];
+var nav_sections = [$("#nav_landing"), $("#nav_about"), $("#nav_contact")];
+
+$("#nav_landing").click(function(){
+    hideSections($("#landing"));
+    updateNav(this);
+});
+
+$("#nav_about").click(function(){
+    hideSections($("#about"))
+    updateNav(this);
+});
+
+$("#nav_contact").click(function(){
+    hideSections($("#contact"))
+    updateNav(this);
+});
+
+var updateNav = function (current) {
+    for (let sect of nav_sections){
+        sect.removeClass("current_nav");
+    }
+    
+    $(current).addClass("current_nav");
+}
+
+var hideSections = function (current) {
+    for (let sect of sections){
+        sect.css("display", "none");
+    }
+
+    current.css("display", "flex");
+}
+
+$("#nav_landing").click();
