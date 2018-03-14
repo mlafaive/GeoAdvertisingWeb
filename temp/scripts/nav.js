2b7b104e34139e5dec3aaaf8aee164148725a04b
kwokh <kwokh@umich.edu>
2018-03-14 02:37:35 -0400

    more to non-signed on pages

--- temp/scripts/nav.js
+++ temp/scripts/nav.js
@@ -1,35 +1,40 @@
-var sections = [$("#landing"), $("#about"), $("#contact")];
-var nav_sections = [$("#nav_landing"), $("#nav_about"), $("#nav_contact")];
+var sections = [$("#landing"), $("#about"), $("#contact"), $("#login")];
+var nav_sections = [$(".nav_landing"), $(".nav_about"), $(".nav_contact"), $(".nav_login")];
 
-$("#nav_landing").click(function(){
+$(".nav_landing").click(function () {
     hideSections($("#landing"));
     updateNav(this);
 });
 
-$("#nav_about").click(function(){
+$(".nav_about").click(function () {
     hideSections($("#about"))
     updateNav(this);
 });
 
-$("#nav_contact").click(function(){
+$(".nav_contact").click(function () {
     hideSections($("#contact"))
     updateNav(this);
 });
 
+$(".nav_login").click(function () {
+    hideSections($("#login"))
+    updateNav(this);
+});
+
 var updateNav = function (current) {
-    for (let sect of nav_sections){
+    for (let sect of nav_sections) {
         sect.removeClass("current_nav");
     }
-    
+
     $(current).addClass("current_nav");
 }
 
 var hideSections = function (current) {
-    for (let sect of sections){
+    for (let sect of sections) {
         sect.css("display", "none");
     }
 
     current.css("display", "flex");
 }
 
-$("#nav_landing").click();
+$(".nav_landing").click();
