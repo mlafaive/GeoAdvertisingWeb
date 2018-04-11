318bddd39460870ff1cfb1c4509a63a635aeb97f
kwokh <kwokh@umich.edu>
2018-04-11 16:30:13 -0400

    Navigation bar for businesses

--- src/components/business-nav.vue
+++ src/components/business-nav.vue
@@ -1,38 +1,48 @@
 <template>
-<b-navbar toggleable="md" type="light" variant="light">
+  <b-navbar toggleable="md" type="light" variant="light" v-if="business">
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-navbar-brand href="#">
+      <router-link exact :to="{ name: 'business', params: { id: business_id } }"> 
+        {{ business.name }}
+      </router-link>
+    </b-navbar-brand>
 
-  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-collapse is-nav id="nav_collapse">
 
-  <b-navbar-brand href="#">
-    <router-link exact to="#">{{ business_name }}</router-link>
-  </b-navbar-brand>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">Advertisements</b-nav-item>
+        <b-nav-item href="#">Business Settings</b-nav-item>
+      </b-navbar-nav>
 
-  <b-collapse is-nav id="nav_collapse">
-
-    <!-- Right aligned nav items -->
-    <b-navbar-nav class="ml-auto">
-      <b-nav-item href="#">Advertisements</b-nav-item>
-      <b-nav-item href="#">Business Settings</b-nav-item>
-    </b-navbar-nav>
-
-  </b-collapse>
-</b-navbar>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
 export default {
   data: function() {
     return {
-      business_name: "mystery"
+      business: {},
+      business_id: this.$route.params.id
     };
   },
   methods: {
-    getActive: function() {
-      console.log(this.$route.params);
+    getBusiness: function() {
+      var url = `businesses/${this.business_id}/`;
+      this.$http
+        .get(url)
+        .then(data => {
+          this.business = data.body;
+
+          console.log("offers");
+          console.log(this.business.offers);
+        })
+        .catch(err => {});
     }
   },
   mounted() {
-    this.getActive();
+    this.getBusiness();
   }
 };
 </script>
--- src/routes/business.vue
+++ src/routes/business.vue
@@ -1,53 +1,23 @@
 <template>
 	<div>
-        <!--
-		<BusinessDashboard v-if="businesses === null || businesses.length"></BusinessDashboard>
-		<NewBusinessDashboard v-else></NewBusinessDashboard>
-		{{businesses}} what is this
-        -->
     <BusinessNav></BusinessNav>
+
+    <!-- CreateOffer -->
+    <!-- Current Offers -->
 	</div>
 </template>
 
 <script>
 import BusinessNav from "../components/business-nav.vue";
-/*
-import NewBusinessDashboard from "../components/new-business-dashboard.vue";
-import BusinessDashboard from "../components/business-dashboard.vue";
-*/ 
 
 export default {
   components: {
     BusinessNav
   },
   data: function() {
-    return {
-      businesses: null
-    };
+    return {};
   },
-  methods: {
-    getActive: function() {
-      console.log(this.$route.params);
-        /*
-      var url = `users/${this.$store.state.email}/businesses`;
-      this.$http
-        .get(url)
-        .then(data => {
-          this.businesses = data.body.businesses;
-        })
-        .catch(err => {
-          console.error(err);
-        });
-        */
-    },
-
-    getUpcoming: function() {
-      console.log("get upcoming");
-    }
-  },
-  mounted() {
-    this.getActive();
-    this.getUpcoming();
-  }
+  methods: { },
+  mounted() { }
 };
 </script>
