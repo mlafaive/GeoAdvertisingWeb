5e4052bbef7c3801e27f3acd127122561e2ab70b
kwokh <kwokh@umich.edu>
2018-04-11 13:50:16 -0400

    nav for selected business

---
+++ src/routes/business.vue
@@ -0,0 +1,52 @@
+<template>
+	<div>
+        <!--
+		<BusinessDashboard v-if="businesses === null || businesses.length"></BusinessDashboard>
+		<NewBusinessDashboard v-else></NewBusinessDashboard>
+		{{businesses}} what is this
+        -->
+	</div>
+</template>
+
+<script>
+import BusinessNav from "../components/business-nav.vue";
+/*
+import NewBusinessDashboard from "../components/new-business-dashboard.vue";
+import BusinessDashboard from "../components/business-dashboard.vue";
+*/ 
+
+export default {
+  components: {
+    BusinessNav,
+    BusinessDashboard
+  },
+  data: function() {
+    return {
+      businesses: null
+    };
+  },
+  methods: {
+    getActive: function() {
+        /*
+      var url = `users/${this.$store.state.email}/businesses`;
+      this.$http
+        .get(url)
+        .then(data => {
+          this.businesses = data.body.businesses;
+        })
+        .catch(err => {
+          console.error(err);
+        });
+        */
+    },
+
+    getUpcoming: function() {
+      console.log("get upcoming");
+    }
+  },
+  mounted() {
+    this.getActive();
+    this.getUpcoming();
+  }
+};
+</script>
