1d024a38a6fef941afc719ae80e1333b4ae9357b
kwokh <kwokh@umich.edu>
2018-04-14 16:16:45 -0400

    offer page update

--- src/routes/offer-info.vue
+++ src/routes/offer-info.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>    
-    <h1>Offer information page</h1>
+    <h1 class='mt-2 font-weight-light d-inline-block'>{{offer.description}}</h1>
 	</div>
 </template>
 
@@ -10,9 +10,27 @@ export default {
   components: {
   },
   data: function() {
-    return {};
+    return {
+      business_id: this.$route.params.id,
+      offer_id: this.$route.params.oid,
+      offer: {}
+    };
   },
-  methods: { },
-  mounted() { }
+  methods: {
+    getOffer: function() {
+      var url = `offers/${this.offer_id}/`;
+      this.$http
+        .get(url)
+        .then(data => {
+          this.offer = data.body;
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
+  },
+  mounted() { 
+    this.getOffer();
+  }
 };
 </script>
