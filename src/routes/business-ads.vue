0cb73e92878d987d28e4a252d56b848e71075663
kwokh <kwokh@umich.edu>
2018-04-15 01:07:55 -0400

    status & search filter on all ads page

--- src/routes/business-ads.vue
+++ src/routes/business-ads.vue
@@ -1,7 +1,17 @@
 <template>
 	<div>
     <h1>Offers</h1>
-    <b-table hover :items="offers" :fields="ad_fields" @row-clicked="clicked"></b-table>
+    <b-form-input v-model="filter"
+                  type="text"
+                  placeholder="Search advertisements">
+    </b-form-input>
+                  <br>
+
+    <b-table hover :items="offers" :fields="ad_fields" 
+             :filter="filter" @row-clicked="clicked"
+     sort-by="status"
+     sort-order="asc"
+     ></b-table>
 	</div>
 </template>
 
@@ -13,6 +23,7 @@ export default {
   },
   data: function() {
     return {
+      filter: "",
       business_id: this.$route.params.id,
       offers: [],
       ad_fields: {
@@ -27,6 +38,10 @@ export default {
         end_time: {
           label: 'End Time',
           sortable: true
+        },
+        status: {
+          label: 'Status',
+          sortable: true
         }
       }
     };
@@ -39,6 +54,19 @@ export default {
       var id = item.id
       this.$router.push({ name: 'business-offer', params: { id: this.business_id, oid: id }})
       
+    },    
+    offerStatus: function(start_d, end_d) {
+      let now = new Date()      
+      let start = new Date(start_d)
+      let end = new Date(end_d)
+
+      if (end <= now) {
+        return "Inactive"
+      } else if (start > now) {
+        return "Scheduled"
+      } else {
+        return "Active"
+      }
     },
     getOffers: function() {
       var url = `businesses/${this.business_id}/offers`;
@@ -47,6 +75,8 @@ export default {
         .then(data => {
             this.offers = data.body.offers;
             for (var i = 0; i < this.offers.length; i++) { 
+                this.offers[i].status = this.offerStatus(this.offers[i].start_time, this.offers[i].end_time);
+                
                 this.offers[i].start_time= this.formatDate(this.offers[i].start_time);
                 this.offers[i].end_time= this.formatDate(this.offers[i].end_time);
             }
